67cb23839b74bec43cce98ff328157bee4c538f1
SpokeSmile <82218302+SpokeSmile@users.noreply.github.com>
2026-05-04 14:09:41 +0300

    link core with views

--- coreApp/fileWork.py
+++ coreApp/fileWork.py
@@ -3,9 +3,9 @@ import tkinter.filedialog as tkfd
 
 def open_file() -> str:
     file_path = tkfd.askopenfilename()
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
     
 
 def menu_open_file(content_text):
-    content_text.insert(0, open_file())
+    content_text.insert(1.0, open_file())
--- views/mainWindow.py
+++ views/mainWindow.py
@@ -1,17 +1,20 @@
 import tkinter as tk
-
+from coreApp import fileWork as fw
 
 window = tk.Tk()
+window.config(width=600,height=600)
+
+
 
 content = tk.Text(window)
-content.place(relx=1,rely=1)
+content.place(x=0,y=0, relwidth=1,relheight=1)
 
 
 
 main_menu = tk.Menu(window)
 
 file_menu = tk.Menu(main_menu)
-file_menu.add_command(label="Открыть файл")
+file_menu.add_command(label="Открыть файл", command=lambda text=content: fw.menu_open_file(text))
 
 
 
